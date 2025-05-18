e6cf5e3549f0c18ee30cc14f9944f84067bbdc7e
mountbook <mountbook@mountbooknoMacBook-Air.local>
2025-05-18 11:17:35 +0900

    250518_必須仕様まで作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,12 @@ class Post < ApplicationRecord
  validates :begin, presence: true
  validates :end, presence: true
  validates :schedule_memo, length: {maximum: 500}
+ validate :enddate_earlier_than_begindate
 
-
+  private
+    def enddate_earlier_than_begindate
+      if self[:end] < self[:begin]
+        errors.add(:end, "は開始日以降の日付にしてください")
+      end
+    end
 end
