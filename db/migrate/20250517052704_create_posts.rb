416b22efea85dcc778462dc5a94398ebd09b4eb0
mountbook <mountbook@mountbooknoMacBook-Air.local>
2025-05-20 12:42:05 +0900

    250520 指摘事項修正

--- db/migrate/20250517052704_create_posts.rb
+++ db/migrate/20250517052704_create_posts.rb
@@ -5,7 +5,7 @@ class CreatePosts < ActiveRecord::Migration[6.1]
       t.date :begin
       t.date :end
       t.string :allday
-      t.string :schedule_memo #後からカラムを追加して、後から書き加えたコードになる
+      #t.string :schedule_memo (追記)２重追加のためコメントアウト（＝削除）→元は後からカラムを追加して、後から書き加えたコードになる
       t.timestamps
     end
   end
